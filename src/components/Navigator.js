926a7f1c6ab67a279d27c3c42b27b184b67c2911
nmarias_dev <nicolasarias870@gmail.com>
2019-03-25 09:22:17 -0500

    add router and folder organization

---
+++ src/components/Navigator.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navigator extends Component {
+  render() {
+    return <h4>I'm the navigator</h4>;
+  }
+}
+
+export default Navigator;
