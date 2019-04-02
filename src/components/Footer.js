671f85a39f36c63344213050d273cceb07ba0348
nmarias_dev <nicolasarias870@gmail.com>
2019-04-01 22:44:13 -0500

    cambios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./Footer.module.css";
-
+import Logo from "./../assets/logo.svg";
 function Footer({}) {
-  return <footer>I'm a cool footer</footer>;
+  return (
+    <footer>
+      <div className={styles.Brand}>
+        <img src={Logo} />
+        <h4>Nicolás Arias</h4>
+        <strong>Fullstack developer</strong>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
